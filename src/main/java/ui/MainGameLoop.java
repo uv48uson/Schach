b875a1312b0576f7cf8b833bdda61664b4ad5b44
Captain Sparrow <Captain Sparrow@192.168.178.58>
2016-07-19 00:29:19 +0200

    Req-01: Graphics LWJGL 6

--- src/main/java/ui/MainGameLoop.java
+++ src/main/java/ui/MainGameLoop.java
@@ -1,12 +1,15 @@
 package ui;
 
+import models.RawModel;
+import models.TexturedModel;
+
 import org.lwjgl.opengl.Display;
 
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
 import renderEngine.Renderer;
 import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
 
@@ -30,13 +33,22 @@ public class MainGameLoop {
 				3,1,2
 		};
 		
-		RawModel model = loader.loadToVAO(vertices, indices);
+		float[] textureCoords = {
+				0,0,
+				0,1,
+				1,1,
+				1,0
+		};
+		
+		RawModel model = loader.loadToVAO(vertices,textureCoords, indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("Chessman.png"));
+		TexturedModel texturedModel = new TexturedModel(model, texture);
 		
 		while(!Display.isCloseRequested()){
 			//game logic
 			renderer.prepare();
 			shader.start();			
-			renderer.render(model);
+			renderer.render(texturedModel);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
